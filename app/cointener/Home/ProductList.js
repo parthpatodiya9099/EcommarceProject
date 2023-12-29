73fcf0beeca6959c2ea19085df80d9526ee73871
parthpatodiya9099 <parthpatodiya76@gmail.com>
2023-12-29 11:12:43 +0530

    28/12/2023

--- app/cointener/Home/ProductList.js
+++ app/cointener/Home/ProductList.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, TouchableOpacity, Modal, StyleSheet } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Modal, StyleSheet, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { horizontalScale, moderateScale, verticalScale } from '../../Constant/Metrics'
 import Feather from 'react-native-vector-icons/Feather';
@@ -9,47 +9,94 @@ import CategariCard from '../../component/Card/CategariCard';
 import AppButton from '../../component/Button/AppButton';
 import ProductCard from '../../component/ProductCard';
 
-
+let arr = [];
 export default function ProductList({ navigation }) {
+
   const [model, Setmodel] = useState(false)
   const [productData, SetProductData] = useState([])
+  const [search, Setsearch] = useState('')
+  const [sort, setSort] = useState('')
+  const [category, SetCategory] = useState('')
+
   const handlepress = () => {
     Setmodel(true)
   }
   const handleclose = () => {
     Setmodel(false)
   }
+
   useEffect(() => {
     HandleData();
   }, []);
   const HandleData = async () => {
     const responce = await fetch('https://api.escuelajs.co/api/v1/products')
     const Data = await responce.json();
-    console.log(Data);
     SetProductData(Data)
   }
+
+  const SearchsortData = () => {
+    let fdata = productData.filter((v) =>
+      v.title.toLowerCase().includes(search.toLowerCase())
+    )
+    if (category !== '') {
+      fdata = productData.filter((v) => category == v.category.name)
+    }
+    fdata = fdata.sort((a, b) => {
+      if (sort === "AZ") {
+        return (a.title > b.title ? 1 : -1)
+      } else if (sort === "ZA") {
+        return (a.title < b.title ? 1 : -1)
+      } else if (sort === "hl") {
+        return (b.price - a.price)
+      } else if (sort === "lh") {
+        return (a.price - b.price)
+      } else {
+        return (a.title > b.title ? 1 : -1)
+      }
+    })
+
+
+    return fdata;
+  }
+  const finleData = SearchsortData()
+
+
+
   return (
     <View>
+      {
+        finleData.map((v) => {
+          if (!arr.includes(v.category.name)) {
+            arr.push(v.category.name)
+          }
+        })
+      }
 
-      <View style={{ width: '100%', height: verticalScale(50), flexDirection: 'row', padding: 5 }}>
+
+      {/* category */}
+
+      <View style={style.contener}>
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <TouchableOpacity style={{ width: horizontalScale(110), height: verticalScale(35), backgroundColor: 'black', borderRadius: moderateScale(20), marginLeft: horizontalScale(10) }}>
-            <Text style={{ fontSize: 18, color: 'white', textAlign: 'center', padding: 6 }}>T-shirts</Text>
+          <TouchableOpacity style={style.categorybox} onPress={() => SetCategory('')}>
+            <Text style={{ fontSize: 18, color: 'white', textAlign: 'center', padding: 6 }}>All</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{ width: horizontalScale(110), height: verticalScale(35), backgroundColor: 'black', borderRadius: moderateScale(20), marginLeft: horizontalScale(10) }}>
-            <Text style={{ fontSize: 18, color: 'white', textAlign: 'center', padding: 6 }}>Crop-tops</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{ width: horizontalScale(110), height: verticalScale(35), backgroundColor: 'black', borderRadius: moderateScale(20), marginLeft: horizontalScale(10) }}>
-            <Text style={{ fontSize: 18, color: 'white', textAlign: 'center', padding: 6 }}>Blouses</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{ width: horizontalScale(110), height: verticalScale(35), backgroundColor: 'black', borderRadius: moderateScale(20), marginLeft: horizontalScale(10) }}>
-            <Text style={{ fontSize: 18, color: 'white', textAlign: 'center', padding: 6 }}>Shirts</Text>
-          </TouchableOpacity>
+          {
+            arr.map((v, i) => {
+              return (
+                <TouchableOpacity style={style.categorybox} key={i} onPress={() => SetCategory(v)}>
+                  <Text style={{ fontSize: 18, color: 'white', textAlign: 'center', padding: 6 }}>{v}</Text>
+                </TouchableOpacity>
+              )
+            })
+          }
 
         </ScrollView>
 
       </View>
-      <View style={{ width: '100%', height: verticalScale(35), flexDirection: 'row', padding: 5, }}>
+
+      {/* Filter */}
+
+      <View style={style.filterbox}>
         <TouchableOpacity style={{ marginLeft: horizontalScale(15) }} onPress={() => (navigation.navigate('Filter'))}>
           <Feather name='filter' color='black' size={25} />
         </TouchableOpacity>
@@ -61,31 +108,48 @@ export default function ProductList({ navigation }) {
           <AntDesign name='swap' color='black' size={25} />
         </TouchableOpacity>
         <TouchableOpacity onPress={() => handlepress()}>
-          <Text style={{ fontSize: moderateScale(18), marginLeft: verticalScale(7), marginTop: verticalScale(2) }}>Price:Lower To high</Text>
+          <Text style={{ fontSize: moderateScale(18), marginLeft: verticalScale(7), marginTop: verticalScale(2) }}>Price:High To Low</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={{ marginLeft: horizontalScale(50) }}>
+        <TouchableOpacity style={{ marginLeft: horizontalScale(50), flexShrink: 1 }}>
           <Feather name='list' color='black' size={25} />
         </TouchableOpacity>
       </View>
-      <ScrollView style={{marginBottom:verticalScale(80),}}>
-        <View style={{width:'90%',marginLeft:horizontalScale(15),flexDirection:'row',flexWrap:'wrap',justifyContent:'space-between'}}>
+
+      {/* Searchbox */}
+
+      <View>
+        <TextInput
+          placeholder='Search...'
+          style={style.search}
+          onChangeText={Setsearch}
+          value={search}
+        />
+      </View>
+
+      {/* ProductCard */}
+
+      <ScrollView style={{ marginBottom: verticalScale(80), }}>
+        <View style={style.productcardbox}>
           {
-            productData.map((v,i)=>{
-              return(
+            finleData.map((v, i) => {
+              return (
                 <ProductCard
-                imgurl={v.images[0]}  
-                dis={v.description}
-                titel={v.title}
-                price={v.price}
-                onPress={()=>navigation.navigate('ProductDetails')}
-              />
+                  key={v.id}
+                  imgurl={v.images[0]}
+                  title={v.title}
+                  price={v.price}
+                  onPress={() => navigation.navigate('ProductDetails')}
+                />
               )
             })
           }
-         
+
         </View>
       </ScrollView>
+
+      {/* Modal */}
+
       <View>
         <Modal
           animationType='slide'
@@ -93,26 +157,26 @@ export default function ProductList({ navigation }) {
           visible={model}
         >
           <View style={style.modlestyle}>
-            <TouchableOpacity style={{ width: horizontalScale(90), height: verticalScale(7), backgroundColor: 'gray', marginLeft: horizontalScale(140) }} onPress={() => handleclose()}>
+            <TouchableOpacity style={style.closemodal} onPress={() => handleclose()}>
               <Feather name='minus' color={'black'} size={moderateScale(40)} />
             </TouchableOpacity>
-            <Text style={{ fontSize: moderateScale(20), color: 'black', marginLeft: horizontalScale(157), marginTop: verticalScale(15) }}>Sort by</Text>
+            <Text style={style.titeltext}>Sort by</Text>
 
-            <View style={{ width: '100%', height: '100%', marginTop: verticalScale(30), }}>
-              <TouchableOpacity style={{ width: '100%', height: verticalScale(55) }}>
-                <Text style={{ fontSize: moderateScale(22), marginTop: verticalScale(14), marginLeft: horizontalScale(20), color: 'black', }}>Popular</Text>
+            <View style={style.sortboxmodal}>
+              <TouchableOpacity style={{ width: '100%', height: verticalScale(55) }} onPress={() => Setmodel(false)}>
+                <Text style={style.sortingtext}>Popular</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{ width: '100%', height: verticalScale(55), borderWidth: 0.5 }}>
-                <Text style={{ fontSize: moderateScale(22), marginTop: verticalScale(14), marginLeft: horizontalScale(20), color: 'black' }}>Newest</Text>
+              <TouchableOpacity style={style.sortingbox} onPress={() => { (setSort('AZ')), Setmodel(false) }}>
+                <Text style={style.sortingtext}>A To Z</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{ width: '100%', height: verticalScale(55), borderWidth: 0.5 }}>
-                <Text style={{ fontSize: moderateScale(22), marginTop: verticalScale(14), marginLeft: horizontalScale(20), color: 'black' }}>Customer review</Text>
+              <TouchableOpacity style={style.sortingbox} onPress={() => { (setSort('ZA')), Setmodel(false) }}>
+                <Text style={style.sortingtext}>Z To A</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{ width: '100%', height: verticalScale(55), backgroundColor: '#DB3022', borderWidth: 0.5 }}>
-                <Text style={{ fontSize: moderateScale(22), marginTop: verticalScale(14), marginLeft: horizontalScale(20), color: 'black' }}>Price: lowest to high</Text>
+              <TouchableOpacity style={style.sortingbox} onPress={() => { (setSort('lh')), Setmodel(false) }}>
+                <Text style={style.sortingtext}>Price: lowest to high</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={{ width: '100%', height: verticalScale(55), borderWidth: 0.5 }}>
-                <Text style={{ fontSize: moderateScale(22), marginTop: verticalScale(14), marginLeft: horizontalScale(20), color: 'black' }}>Price: highest to low</Text>
+              <TouchableOpacity style={style.sortingbox} onPress={() => { (setSort('hl')), Setmodel(false) }}>
+                <Text style={style.sortingtext}>Price: highest to low</Text>
               </TouchableOpacity>
             </View>
 
@@ -134,4 +198,63 @@ const style = StyleSheet.create({
     elevation: 9,
     marginTop: verticalScale(400)
   },
+  search: {
+    width: '89%',
+    marginLeft: horizontalScale(20),
+    borderBottomWidth: 0.5,
+  },
+  contener: {
+    width: '100%',
+    height: verticalScale(50),
+    flexDirection: 'row',
+    padding: 5
+  },
+  categorybox: {
+    width: horizontalScale(110),
+    height: verticalScale(35),
+    backgroundColor: 'black',
+    borderRadius: moderateScale(20),
+    marginLeft: horizontalScale(10)
+  },
+  filterbox: {
+    width: '100%',
+    height: verticalScale(35),
+    flexDirection: 'row',
+    padding: 5,
+  },
+  productcardbox: {
+    width: '90%',
+    marginLeft: horizontalScale(15),
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  closemodal: {
+    width: horizontalScale(90),
+    height: verticalScale(7),
+    backgroundColor: 'gray',
+    marginLeft: horizontalScale(140)
+  },
+  sortboxmodal: {
+    width: '100%',
+    height: '100%',
+    marginTop: verticalScale(30),
+  },
+  sortingbox: {
+    width: '100%',
+    height: verticalScale(55),
+    borderWidth: 0.5
+  },
+  sortingtext: {
+    fontSize: moderateScale(22),
+    marginTop: verticalScale(14),
+    marginLeft: horizontalScale(20),
+    color: 'black'
+  },
+  titeltext: {
+    fontSize: moderateScale(20),
+    color: 'black',
+    marginLeft: horizontalScale(157),
+    marginTop: verticalScale(15)
+  }
 })
--- app/component/ProductCard.js
+++ app/component/ProductCard.js
@@ -1,37 +1,52 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { horizontalScale, moderateScale, verticalScale } from '../Constant/Metrics'
 import Feather from 'react-native-vector-icons/Feather';
 
-export default function ProductCard({imgurl,dis,titel,price,onPress}) {
+export default function ProductCard({ imgurl, title, price, onPress }) {
     return (
         <View>
             <View style={{ margin: 5 }}>
-                <TouchableOpacity style={{ width: horizontalScale(150), height: verticalScale(200), borderWidth: 0.5, borderRadius: moderateScale(10),marginTop:verticalScale(10) }} onPress={onPress}>
+                <TouchableOpacity style={style.imgbox} onPress={onPress}>
                     <Image
-                        source={{uri:imgurl.toString().substring(0,8) !== 'https://' ? 'https://i.imgur.com/eGOUveI.jpeg' :imgurl}}
-                        style={{ width: '100%', height: '100%', borderRadius: moderateScale(10) }}
+                        source={{ uri: imgurl.toString().substring(0, 8) !== 'https://' ? "https://i.imgur.com/OKn1KFI.jpeg" : imgurl }}
+                        style={style.img}
                     />
-                     {/* <TouchableOpacity style={{ width: horizontalScale(37), height:verticalScale(37), borderWidth: 0.5, borderColor: "black", marginLeft: horizontalScale(115), borderRadius: moderateScale(160), backgroundColor: 'white', padding: 9, paddingRight: horizontalScale(4),marginTop:verticalScale(300),position:'relative' }}>
-                        <Feather name="heart" color='black' size={moderateScale(18)} />
-                    </TouchableOpacity> */}
                 </TouchableOpacity>
-                <View style={{ flexDirection: 'row', marginTop: verticalScale(5) }}>
+                <View style={style.starbox}>
                     <Feather name="star" color='#FFBA49' size={moderateScale(16)} />
                     <Feather name="star" color='#FFBA49' size={moderateScale(16)} />
                     <Feather name="star" color='#FFBA49' size={moderateScale(16)} />
                     <Feather name="star" color='#FFBA49' size={moderateScale(16)} />
                     <Feather name="star" color='#FFBA49' size={moderateScale(16)} />
-                    <Text>(10)</Text>             
+                    <Text>(10)</Text>
                 </View>
-                <View style={{width: horizontalScale(150)}}>
-                <Text>{dis}</Text>
-                <Text style={{fontSize:19,color:'black'}}>{titel}</Text>
-                <Text style={{fontSize:18,color:'black'}}>${price}</Text>
+                <View style={{ width: horizontalScale(150) }}>
+                    <Text style={{ fontSize: 19, color: 'black' }}>{title}</Text>
+                    <Text style={{ fontSize: 18, color: 'black' }}>${price}</Text>
                 </View>
-                
+
             </View>
 
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    imgbox: {
+        width: horizontalScale(150),
+        height: verticalScale(200),
+        borderWidth: 0.5,
+        borderRadius: moderateScale(10),
+        marginTop: verticalScale(10)
+    },
+    img: {
+        width: '100%',
+        height: '100%',
+        borderRadius: moderateScale(10)
+    },
+    starbox: {
+        flexDirection: 'row',
+        marginTop: verticalScale(5)
+    }
+})
